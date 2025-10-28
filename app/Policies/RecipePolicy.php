dcc8f0e4a37ad889770b4dce0c916bb5ec0537c5
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-10-28 09:47:52 -0500

    starts recipe service

--- app/Policies/RecipePolicy.php
+++ app/Policies/RecipePolicy.php
@@ -2,15 +2,66 @@
 
 namespace App\Policies;
 
+use App\Models\Recipe;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class RecipePolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any models.
      */
-    public function __construct()
+    public function viewAny(User $user): bool
     {
-        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Recipe $recipe): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Recipe $recipe): bool
+    {
+        //Check if the user is the owner of the recipe
+        return $user->id === $recipe->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Recipe $recipe): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Recipe $recipe): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Recipe $recipe): bool
+    {
+        return false;
     }
 }
